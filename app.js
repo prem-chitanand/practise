2fe97b2258333e3cdc32a84158b3a46f55e8f1b5
prem-chitanand <prem.chitanand@gmail.com>
2021-10-11 19:30:25 +0530

    file system

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// console.log(module);
-// console.log(require('./index'))
-const stuff= require('./utils')
-const {add, sub, multiply, divide } = require('./utils')
-add()
-sub()
-multiply()
-divide()
+const fs=require('fs');
+fs.readFile('./test.txt',(error,data) => {
+   if(error) console.log(error)
+   else console.log(data.toString())
+})
+console.log(fs.readFileSync('./test.txt','utf-8'));
+console.log("im a random");
+fs.writeFile('./test.txt',"new stuff added inside",(error,data) => {
+   if(error) console.log(error)
+})
