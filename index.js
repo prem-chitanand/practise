4884aacdfc495d13218da10de8885ecada26c4b0
prem-chitanand <prem.chitanand@gmail.com>
2021-10-11 18:59:16 +0530

    path modules

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-console.log("hello world");
-const greet = () => 'hello'
-module.exports.greet=greet
-console.log("hello greet");
-// console.log(module)
+const path=require('path');
+// console.log(path);
+console.log(path.resolve('./utils.js'));
+console.log(path.relative('./temp',"/temp/utils.js"))
+console.log(path.extname('./app.js'));
+console.log(path.dirname('./temp/utils.js'));
+console.log(__dirname);
+console.log(path.join(__dirname,'app.js'))
